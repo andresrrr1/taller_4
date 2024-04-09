670f25fc27fbc1840f60a00cc5697933bff92efa
andres romero <andresromero5455@gmail.com>
2024-04-09 15:35:28 -0500

    taller

--- taller_4.py
+++ taller_4.py
@@ -1,1 +1,23 @@
-import numpy
+import numpy as np
+
+# milista=[3,5,7,9]
+# miarreglo=np.array(milista)
+# print(miarreglo.ndim)
+# print(miarreglo.shape)
+# print(miarreglo.size)
+# print(miarreglo.dtype)
+""" milista=[3,5,7,9]
+miarreglo2=np.array(milista)
+print(miarreglo2.ndim) """
+""" milista=([1,2,3,4],[3,8,2,4],[2,5,7,9])
+miarreglo=np.array(milista, dtype=int)
+print(miarreglo)
+print(milista) """
+# miarreglo=np.empty((5,4))
+# print(miarreglo)
+""" miarreglo= np.arange((-10,10))
+
+miarreglo=np.reshape((2,10))
+print(miarreglo) """
+miarreglo= np.linspace((0,100,10))
+print(miarreglo.size)
