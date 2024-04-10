7b266129c085b54661abecc53726fde859d2793b
andres romero <andresromero5455@gmail.com>
2024-04-10 14:27:47 -0500

    operaciones

---
+++ arreglo_1.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as ptl
+# a= np.array([5,10,15,25,7])
+# print(a)
+# b= np.array([3,4,7,9,56])
+# print(b)
+
+# c= a**2
+# print(c)
+
+""" a= np.array([5,10,15,20,25])
+print(a)
+b= np.array([3,4,7,9,56])
+print(b)
+
+c= a@b
+print(c) """
+
+""" a= np.array([5,10,15,20,25])
+print(a)
+b= np.array([3,4,7,9,56])
+print(b)
+
+b+=1
+print(b) """
+
+a= np.array([5,10,15,20,25])
+print(a)
+b= np.array([3,4,7,9,56])
+print(b)
+
+c= a/b
+print(c)
